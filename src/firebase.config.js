a21d2214d56b32ee5008b5191408714e1c24f741
Niels Uitterdijk <niels.uit@gmail.com>
2023-05-16 15:55:44 +0200

    add firestore

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
@@ -7,12 +7,15 @@ const firebaseConfig = {
   authDomain: "learnalanguage-63443.firebaseapp.com",
   projectId: "learnalanguage-63443",
   storageBucket: "learnalanguage-63443.appspot.com",
+  databaseURL: "https://learnalanguage-63443.firebaseio.com",
   messagingSenderId: "981225542967",
   appId: "1:981225542967:web:1bb78cd325f32ec870f6b2",
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth();
 export const db = getFirestore();
+
+connectFirestoreEmulator(db, "127.0.0.1", 8080);
